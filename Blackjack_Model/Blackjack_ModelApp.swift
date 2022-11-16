ea83696a3ce1487e7e236a7c681815b08c403dd0
ValWeinhaeupl <val.weinhaeupl@gmail.com>
2022-11-16 11:40:09 +0100

    initial commit

---
+++ Blackjack_Model/Blackjack_ModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blackjack_ModelApp.swift
+//  Blackjack_Model
+//
+//  Created by Valentin Weinhaeupl on 09.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Blackjack_ModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
