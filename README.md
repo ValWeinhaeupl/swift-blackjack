76fab7ea2f09ea33aeb1c4559bad8ad81f0242f2
ValWeinhaeupl <val.weinhaeupl@gmail.com>
2022-11-16 11:43:22 +0100

    final

--- README.md
+++
@@ -1,3 +0,0 @@
-# BlackjackModel
-
-A description of this package.
--- Sources/BlackjackModel/BlackjackModel.swift
+++
@@ -1,6 +0,0 @@
-public struct BlackjackModel {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
